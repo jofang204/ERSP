eaf8ad61d8aa10f734c03d6ecd4eff2b64214f3f
Yutong Qiu <yuq005@ucsd.edu>
2017-02-26 20:03:07 -0800

    index file prepared

--- soft_parser.py
+++ soft_parser.py
@@ -7,13 +7,18 @@ import re
 
 def parse(fname):
     for l in open(fname):
-        yield str(l)               
+        yield str(l)              
 
 data = parse('./GSE9576_family.soft')
 
 flag = 1
 descripFlag = False
 sampleFlag = False
+
+gene_title = "Gene Title"
+gene_symbol = "Gene Symbol"
+title_ind = 0
+symbol = 0
 
 # dictionary of probe id/gene's
 # probe id/gene --> description
@@ -25,12 +30,12 @@ sampleID = {}
 # empty array
 empty = [] 
 
-# contains lists of valyes (an array of array)
-#info = [] 
-
 # stores all values for a sample 
 temp_id = []
 temp = []
+
+# stores file pointer
+fileptr = {}
 
 id_string = ''
 
@@ -41,23 +46,28 @@ for line in data:
         id_string = line.strip('^SAMPLE = ')
         id_string = id_string.strip('\n')
         sampleID.update({id_string : empty})
+        fileptr[id_string] = []
         continue
 
     # gets gene descriptions
     if '!platform_table_begin\n' in line:
-        line = data.next()
+        line = next(data).split('\t')
+        title_ind = line.index(gene_title)
+        symbol_ind = line.index(gene_symbol)
         descripFlag = True
         continue
+
     elif '!platform_table_end\n' in line:
         descripFlag = False
         continue
 
     # gets expression values for each probe idea in a sample
     elif '!sample_table_begin' in line:
-        line = data.next()
+        line = next(data)
         sampleFlag = True
         temp = [] # CLEAR IT!!
         continue
+
     elif '!sample_table_end' in line:
         sampleFlag = False
         flag = False
@@ -67,13 +77,15 @@ for line in data:
     if descripFlag == True:
         line2 = line.split("\t")
         probeid = line2[0]
-        geneTitle = line2[9]
-        geneSymbol = line2[10]
-        gene_description[probeid] = geneSymbol + ": " + geneTitle
+        geneTitle = line2[title_ind]
+        geneSymbol = line2[symbol_ind]
 
-        # leaves description blank if there isn't one
-        if gene_description[probeid] == ": ":
-            gene_description[probeid] = " "
+        if geneTitle == "":
+            geneTitle = "---"
+        if geneSymbol == "":
+            geneSymbol = "---"
+
+        gene_description[probeid] = geneSymbol + ": " + geneTitle
     
     # grabs the expression value
     if sampleFlag == True:
@@ -82,23 +94,43 @@ for line in data:
         if flag == True:
             temp_id.append(list[0])
 
-# opens file for writing 
-toWrite = open("test.txt", 'w') # specify the output file
+# opens files for writing 
+expr_file = open("GSE9576-expr.txt", 'w+') 
+idx_file = open("GSE9576-idx.txt", 'w+') 
 
-# writes header to output file
-header = "ProbeID" + '\t' + 'Name' + '\t' + '\t'.join([str(i) for i in sampleID])
-toWrite.write(header)
-toWrite.write('\n')
+# writes header to expression file
+expr_header = "ProbeID" + '\t' + 'Name' + '\t' + '\t'.join([str(i) for i in sampleID])
+expr_file.write(expr_header)
+expr_file.write('\n')
 
-for i in range(0,len(temp_id)):
+# writes header to idx file
+idx_header = "ProbeID" + '\t' + 'Ptr' + '\t' + "Name" + '\t' + "Description" + '\n'
+idx_file.write(idx_header)
 
-    # write probe id and description
-    toWrite.write('\t'.join([temp_id[i], gene_description[temp_id[i]]]))
+print(sampleID["GSM242028"][:10])
+
+i = 0
+# write to files
+for id in temp_id:
+    
+    # write idx file
+    idx_file.write('\t'.join([id, str(expr_file.tell()), gene_description[id].split(": ")[0], gene_description[id].split(": ")[1]]))
+
+    # write probe id and description to expression file
+    expr_file.write('\t'.join([id, gene_description[id]]))
     
     # writes each expr value from each GSM
     for element in sampleID.items():
         values = element[1]
-        toWrite.write('\t')
-        toWrite.write(str(values[i]))     
+        expr_file.write('\t')
+        expr_file.write(str(values[i]))
 
-    toWrite.write('\n')
+    expr_file.write('\n')
+    idx_file.write('\n')
+
+    i += 1
+
+position = expr_file.seek(6735);
+str = expr_file.readline()
+
+print(str)
