9bfcfb7365f36d02499a15381760cfd8c91bcef0
Jimmy <swagmaster@jimmys-mbp-2.dynamic.ucsd.edu>
2017-02-28 01:49:08 -0800

    parser creates expr, ih, idx, survival files for GPL570

--- soft_parser.py
+++ soft_parser.py
@@ -1,6 +1,7 @@
+
 # ERSP SAHOO
-# updated 02.21.17
-# Description: Take in a soft file and writes the expression file
+# updated 02.27.17
+# Description: Take in a soft file and writes the expr, idx, ih, survival files
 
 import sys
 import re
@@ -13,35 +14,50 @@ data = parse('./GSE9576_family.soft')
 
 descripFlag = False
 sampleFlag = False
+GSM_id_order = True
+headerFlag = False 
 
-gene_title = "Gene Title"
-gene_symbol = "Gene Symbol"
-title_ind = 0
-symbol = 0
+gene_symbol_strings = ["Gene Symbol"]
+gene_title_strings = ["Gene Title"]
+symbol_index = 0
+title_index = 0
+
+
+# hashes the headers in order to get the gene title and gene symbol
+header_strings = {}
 
 # dictionaries that stores data
 gene_description = {}   # probeid : "symbol: title"
-sampleID = {}   # sampleid: [values]
+sampleID = {}   # GSM numbers
 fileptr = {}    # probeid: fileptr
 
-# probe ids
-probe_id = []
+# list probe_ids in a specific order
+id_order = []
 
-# stroes expr values for each sample
+# order of GSM
+GSM_order = []
+
+# stores expr values for each sample
 temp_values = []
 
+# array for headers (ih file)
+array_header = []
+
+# array for clinical headers (ih file)
+clinical_header = []
+
 #list of headers of channel info/columns
-survivalHeaderList = ["ArrayId", "time", "status", "series"]
+survival_header_list = ["ArrayId", "time", "status", "series"]
 
 #prefix/type of input of the column
-survivalHeaderPre = ["", "", "", "c "]
+survival_header_pre = ["", "", "", "c "]
 
 #declare channel vairables
-channelNum = ""
+channel_num = ""
 channel = ""
 
 #whether keys has been added to the dictionary
-headerAdded = False
+header_added = False
 
 #initialize survival info dictionary
 survival_info = {}
@@ -53,21 +69,138 @@ survival_info["series"] = []
 # gets each probe id and its description 
 for line in data:
 
+    # gets gene descriptions indices
+    if '!platform_table_begin\n' in line:
+        line = data.next()
+        
+        # prepping to get gene decriptions
+        # finds the column indices to get the gene title and gene symbol
+        headers = line.split("\t")
+        
+        # hashes the headers -- each header is mapped to its index in the line
+        for index in xrange(len(headers)):
+            header_strings[headers[index]] = index
+
+        # checks if any common symbol strings are in the header 
+        for idx in xrange(len(gene_symbol_strings)):
+            print gene_symbol_strings[idx] 
+            if header_strings.has_key(gene_symbol_strings[idx]):
+                symbol_index = header_strings.get(gene_symbol_strings[idx])
+                print symbol_index
+
+        # checks if any common title strings are in the header
+        for idx in xrange(len(gene_title_strings)):
+            if header_strings.has_key(gene_title_strings[idx]):
+                title_index = header_strings.get(gene_title_strings[idx])
+                print title_index
+
+        # if no symbol string can be found in header, prompt user to input index 
+        # reprompts user until their input is an integer between the header range
+        if symbol_index == 0:
+            while True:
+                try:
+                    user_input = int(raw_input("Enter an index for gene symbol: "))
+                except ValueError:
+                    print("Please enter an integer!")
+                    continue
+                
+                if user_input >= 0 and user_input <= len(headers):
+                    symbol_index = user_input
+                    break
+                else:
+                    print("Please enter an integer betwen 0 and " + str(len(headers)))
+                    continue
+        
+        # if no title string can be found in header, prompt user to input index 
+        # reprompts user until their input is an integer between the header range
+        if title_index == 0:
+            while True:
+                try:
+                    user_input = int(raw_input("Enter an index for gene title: "))
+                except ValueError:
+                    print("Please enter an integer!")
+                    continue
+                
+                if user_input >= 0 and user_input <= len(headers):
+                    title_index = user_input
+                    break
+                else:
+                    print("Please enter an integer betwen 0 and " + str(len(headers)))
+                    continue
+        
+        descripFlag = True
+        headerFlag = True
+        continue
+
+    # done with gene description
+    elif '!platform_table_end\n' in line:
+        descripFlag = False
+        continue
+
+    # gets expression values for each probe idea in a sample
+    elif '!sample_table_begin' in line:
+        line = data.next()
+        sampleFlag = True
+        temp_values = [] # CLEAR IT!!
+        continue
+    elif '!sample_table_end' in line:
+        sampleFlag = False
+        GSM_id_order = False
+        sampleID.update({id_string : temp_values})
+
+    #find GSE id
+    elif "!Sample_series_id" in line:
+        series_id = line.replace("!Sample_series_id = ", "")
+        series_id = series_id.strip("\n")
+        survival_info["series"].append(series_id)
+    
+    # finds clinical header
+    elif '!Sample_title' in line:
+        c_header = line.strip('!Sample_title = ')
+        c_header = c_header.strip('\n')
+        clinical_header.append(c_header)
+        continue
+
     # gets sample id, initialize sampleID dict
     if '^SAMPLE' in line:
         id_string = line.strip('^SAMPLE = ')
         id_string = id_string.strip('\n')
+        GSM_order.append(id_string)
+        array_header.append(id_string)
         sampleID.update({id_string : []})
         fileptr[id_string] = []
         survival_info["ArrayId"].append(id_string)
         continue
+            
+    # grabs the probe id/gene's description
+    if descripFlag == True:
+        line = line.split('\t')
+        probe_id = line[0]   # gets the probe id which is first column
+        # print probeid
+        gene_symbol = line[symbol_index]
+        gene_title = line[title_index]
+
+        if gene_symbol != "" or gene_title != "":        
+            gene_description[probe_id] = gene_symbol + ": " + gene_title
+        else:
+            gene_description[probe_id] = "..."
+
+    
+    # grabs the expression value
+    if sampleFlag == True:
+        list = re.split(r'\t+', line.rstrip('\t'))
+        temp_values.append(list[1])
+
+        # keep track of correct order of probe ids that appear in soft file
+        if GSM_id_order == True:
+            id_order.append(list[0])
 
     #get the channel index
     if "!Sample_channel_count" in line:
 
         temp = line.strip("!Sample_channel_count = ")
-        channelNum = temp.strip("\n")
-        channel = "_ch"+channelNum
+        channel_num = temp.strip("\n")
+        channel = "_ch"+channel_num
     
         #lines after are with _ch#
         while True:
@@ -75,7 +208,7 @@ for line in data:
             
             #break out when line does not contain _ch#
             if not channel in l:
-                headerAdded = True
+                header_added = True
                 break
             
             #get the header/key
@@ -93,88 +226,91 @@ for line in data:
             
             #if header has not been added, add to dictionary, check its type (char or number)
             #and append to the prefix list
-            if headerAdded == False:
+            if header_added == False:
                 try:
                     n = int(l[1])
-                    survivalHeaderPre.append("n ")
+                    survival_header_pre.append("n ")
                 except ValueError:
-                    survivalHeaderPre.append("c ")
-                survivalHeaderList.append(title)
+                    survival_header_pre.append("c ")
+                survival_header_list.append(title)
                 survival_info[title] = [l[1].strip("\n")]
             else:
                 #append if key already defined
                 survival_info[title].append(l[1].strip("\n"))
 
-    # gets gene descriptions indices
-    if '!platform_table_begin\n' in line:
-        line = data.next().split('\t')
-        title_ind = line.index(gene_title)
-        symbol_ind = line.index(gene_symbol)
-        descripFlag = True
-        continue
-
-    # done with gene description
-    elif '!platform_table_end\n' in line:
-        descripFlag = False
-        continue
-
-    # gets expression values for each probe idea in a sample
-    elif '!sample_table_begin' in line:
-        line = data.next()
-        sampleFlag = True
-        temp_values = [] # CLEAR IT!!
-        continue
-
-    elif '!sample_table_end' in line:
-        sampleFlag = False
-        sampleID.update({id_string : temp_values})
-
-        #find GSE id
-    elif "!Sample_series_id" in line:
-        series_id = line.replace("!Sample_series_id = ", "")
-        series_id = series_id.strip("\n")
-        survival_info["series"].append(series_id)
-            
-    # grabs the probe id/gene's description
-    if descripFlag == True:
-        line2 = line.split("\t")
-        probeid = line2[0]
-        probe_id.append(probeid)
-        geneTitle = line2[title_ind]
-        geneSymbol = line2[symbol_ind]
-
-        #set name or descirption to --- if there is none
-        if geneTitle == "":
-            geneTitle = "---"
-        if geneSymbol == "":
-            geneSymbol = "---"
-
-        gene_description[probeid] = geneSymbol + ": " + geneTitle
-    
-    # grabs the expression value
-    if sampleFlag == True:
-        list = re.split(r'\t+', line.rstrip('\t'))
-        temp_values.append (list[1])
-
 # opens files for writing 
 expr_file = open("GSE9576-expr.txt", 'w+') 
 idx_file = open("GSE9576-idx.txt", 'w')
 survival = open("GSE9576-survival.txt", "w")
+ih_file = open("GSE9576-ih.txt", 'w')
+
+#### EXPR, IH, and IDX WRITING ####
+# writes header to expression file
+expr_header = "ProbeID" + '\t' + 'Name' + '\t' + '\t'.join([str(i) for i in sampleID])
+expr_file.write(expr_header)
+expr_file.write('\n')
+
+# writes header to idx file
+idx_header = "ProbeID" + '\t' + 'Ptr' + '\t' + "Name" + '\t' + "Description" + '\n'
+idx_file.write(idx_header)
+
+
+# write to expr and idx file
+print len(id_order)
+print len(gene_description)
+print len(array_header)
+print len(clinical_header)
+print len(sampleID)
+for i in xrange(len(id_order)):
+    
+    name = id_order[i]
+    
+    # write idx file, record file pointer
+    idx_file.write('\t'.join([name, str(expr_file.tell()), gene_description[name]]))
+
+    # write probe id and description to expression file
+    expr_file.write('\t'.join([name, gene_description[name]]))
+
+    # writes each expr value from each GSM
+    for element in sampleID.items():
+        values = element[1]
+        expr_file.write('\t')
+        expr_file.write(str(values[i]))
+
+    expr_file.write('\n')
+    idx_file.write('\n')
+
+expr_file.close()
+idx_file.close()
+
+## IH WRITING ##
+ih_header = "ArrayID" + '\t' + "ArrayHeader" + '\t' + "ClinicalHeader"
+ih_file.write(ih_header)
+ih_file.write('\n')
+
+# write ih file 
+counter = 0
+for element in GSM_order:
+    ih_file.write('\t'.join([element, array_header[counter], clinical_header[counter]]))
+    ih_file.write('\n')
+    counter += 1
+
+ih_file.close()
 
 ### SURVIVAL WRITING ###
 i = 0
-length = len(survivalHeaderList)
-survivalHeader = ""
+length = len(survival_header_list)
+survival_header = ""
 
 #write header
 for i in range(length):
     #no tab after the last header
     if i == length-1:
-        survivalHeader = survivalHeader + survivalHeaderPre[i] + survivalHeaderList[i]
+        survival_header = survival_header + survival_header_pre[i] + survival_header_list[i]
     else:
-        survivalHeader = survivalHeader + survivalHeaderPre[i] + survivalHeaderList[i] + "\t"
-survivalHeader = survivalHeader + "\n"
-survival.write(survivalHeader)
+        survival_header = survival_header + survival_header_pre[i] + survival_header_list[i] + "\t"
+survival_header = survival_header + "\n"
+survival.write(survival_header)
 
 #write body
 i = 0
@@ -182,7 +318,7 @@ length = len(survival_info["ArrayId"])
 for i in range(length):
     #a line
     aTuple = []
-    for t in survivalHeaderList:
+    for t in survival_header_list:
         #if no value is added to the column, append empty string
         if len(survival_info[t]) == 0:
             aTuple.append("")
@@ -195,36 +331,3 @@ for i in range(length):
     
 survival.close() 
 
-#### EXPR and IDX WRITING ####
-# writes header to expression file
-expr_header = "ProbeID" + '\t' + 'Name' + '\t' + '\t'.join([str(i) for i in sampleID])
-expr_file.write(expr_header)
-expr_file.write('\n')
-
-# writes header to idx file
-idx_header = "ProbeID" + '\t' + 'Ptr' + '\t' + "Name" + '\t' + "Description" + '\n'
-idx_file.write(idx_header)
-
-i = 0
-# write to idx and expr file
-for id in probe_id:
-    
-    # write idx file, record file pointer
-    idx_file.write('\t'.join([id, str(expr_file.tell()), gene_description[id].split(": ")[0], gene_description[id].split(": ")[1]]))
-
-    # write probe id and description to expression file
-    expr_file.write('\t'.join([id, gene_description[id]]))
-
-    # writes each expr value from each GSM
-    for element in sampleID.items():
-        values = element[1]
-        expr_file.write('\t')
-        expr_file.write(str(values[i]))
-
-    expr_file.write('\n')
-    idx_file.write('\n')
-
-    i += 1
-
-idx_file.close()
-expr_file.close()
