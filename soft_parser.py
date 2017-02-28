10f91a3bbe9e6b75e37bd603a60cdc681bbe6c08
Yutong Qiu <yuq005@ucsd.edu>
2017-02-27 17:17:09 -0800

    modified variable names, making code more readable

--- soft_parser.py
+++ soft_parser.py
@@ -11,7 +11,6 @@ def parse(fname):
 
 data = parse('./GSE9576_family.soft')
 
-flag = 1
 descripFlag = False
 sampleFlag = False
 
@@ -20,66 +19,61 @@ gene_symbol = "Gene Symbol"
 title_ind = 0
 symbol = 0
 
-# dictionary of probe id/gene's
-# probe id/gene --> description
-gene_description = {}
+# dictionaries that stores data
+gene_description = {}   # probeid : "symbol: title"
+sampleID = {}   # sampleid: [values]
+fileptr = {}    # probeid: fileptr
 
-# empty dictionary that maps sample to a list of expr values 
-sampleID = {}
+# probe ids
+probe_id = []
 
-# empty array
-empty = [] 
-
-# stores all values for a sample 
-temp_id = []
-temp = []
-
-# stores file pointer
-fileptr = {}
-
-id_string = ''
+# stroes expr values for each sample
+temp_values = []
 
 # gets each probe id and its description 
 for line in data:
 
+    # gets sample id, initialize sampleID dict
     if '^SAMPLE' in line:
         id_string = line.strip('^SAMPLE = ')
         id_string = id_string.strip('\n')
-        sampleID.update({id_string : empty})
+        sampleID.update({id_string : []})
         fileptr[id_string] = []
         continue
 
-    # gets gene descriptions
+    # gets gene descriptions indices
     if '!platform_table_begin\n' in line:
-        line = next(data).split('\t')
+        line = data.next().split('\t')
         title_ind = line.index(gene_title)
         symbol_ind = line.index(gene_symbol)
         descripFlag = True
         continue
 
+    # done with gene description
     elif '!platform_table_end\n' in line:
         descripFlag = False
         continue
 
     # gets expression values for each probe idea in a sample
     elif '!sample_table_begin' in line:
-        line = next(data)
+        line = data.next()
         sampleFlag = True
-        temp = [] # CLEAR IT!!
+        temp_values = [] # CLEAR IT!!
         continue
 
     elif '!sample_table_end' in line:
         sampleFlag = False
-        flag = False
-        sampleID.update({id_string : temp})
+        sampleID.update({id_string : temp_values})
             
     # grabs the probe id/gene's description
     if descripFlag == True:
         line2 = line.split("\t")
         probeid = line2[0]
+        probe_id.append(probeid)
         geneTitle = line2[title_ind]
         geneSymbol = line2[symbol_ind]
 
+        #set name or descirption to --- if there is none
         if geneTitle == "":
             geneTitle = "---"
         if geneSymbol == "":
@@ -90,9 +84,7 @@ for line in data:
     # grabs the expression value
     if sampleFlag == True:
         list = re.split(r'\t+', line.rstrip('\t'))
-        temp.append (list[1])
-        if flag == True:
-            temp_id.append(list[0])
+        temp_values.append (list[1])
 
 # opens files for writing 
 expr_file = open("GSE9576-expr.txt", 'w+') 
@@ -107,18 +99,16 @@ expr_file.write('\n')
 idx_header = "ProbeID" + '\t' + 'Ptr' + '\t' + "Name" + '\t' + "Description" + '\n'
 idx_file.write(idx_header)
 
-print(sampleID["GSM242028"][:10])
-
 i = 0
 # write to files
-for id in temp_id:
+for id in probe_id:
     
     # write idx file
     idx_file.write('\t'.join([id, str(expr_file.tell()), gene_description[id].split(": ")[0], gene_description[id].split(": ")[1]]))
 
     # write probe id and description to expression file
     expr_file.write('\t'.join([id, gene_description[id]]))
-    
+
     # writes each expr value from each GSM
     for element in sampleID.items():
         values = element[1]
@@ -132,5 +122,3 @@ for id in temp_id:
 
 position = expr_file.seek(6735);
 str = expr_file.readline()
-
-print(str)
