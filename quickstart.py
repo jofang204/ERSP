555d8ae72cd4ddef9ffc921b733b6ef6276b5b22
jofang <jofang@hegemon.gc.ucsd.edu>
2017-05-04 00:41:14 -0700

    writes the survival file, needs to write ih file

--- quickstart.py
+++ quickstart.py
@@ -1,4 +1,3 @@
-
 from __future__ import print_function
 import httplib2
 import os
@@ -9,10 +8,10 @@ from oauth2client import tools
 from oauth2client.file import Storage
 
 try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+  import argparse
+  flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
-    flags = None
+  flags = None
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
@@ -22,63 +21,65 @@ APPLICATION_NAME = 'Google Sheets API Python Quickstart'
 
 
 def get_credentials():
-    """Gets valid user credentials from storage.
+  """Gets valid user credentials from storage.
 
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
+  If nothing has been stored, or if the stored credentials are invalid,
+  the OAuth2 flow is completed to obtain the new credentials.
 
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'sheets.googleapis.com-python-quickstart.json')
+  Returns:
+  Credentials, the obtained credential.
+  """
+  home_dir = os.path.expanduser('~')
+  credential_dir = os.path.join(home_dir, '.credentials')
+  if not os.path.exists(credential_dir):
+    os.makedirs(credential_dir)
+  credential_path = os.path.join(credential_dir,
+                                        'sheets.googleapis.com-python-quickstart.json')
 
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+  store = Storage(credential_path)
+  credentials = store.get()
+  if not credentials or credentials.invalid:
+    flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+    flow.user_agent = APPLICATION_NAME
+    if flags:
+      credentials = tools.run_flow(flow, store, flags)
+    else: # Needed only for compatibility with Python 2.6
+      credentials = tools.run(flow, store)
+      print('Storing credentials to ' + credential_path)
+  return credentials
 
 def main():
-    """Shows basic usage of the Sheets API.
+  credentials = get_credentials()
+  http = credentials.authorize(httplib2.Http())
+  discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                        'version=v4')
+  service = discovery.build('sheets', 'v4', http=http,
+                        discoveryServiceUrl=discoveryUrl)
 
-    Creates a Sheets API service object and prints the names and majors of
-    students in a sample spreadsheet:
-    https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-    """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-                    'version=v4')
-    service = discovery.build('sheets', 'v4', http=http,
-                              discoveryServiceUrl=discoveryUrl)
+  spreadsheetId = '1NQjPVqU991QtFjPAOT1tCV1DwSYT-mGnMihW6UA0rz8'
+  rangeName = 'GPL570!E:O'
+  result = service.spreadsheets().values().get(
+                        spreadsheetId=spreadsheetId, range=rangeName).execute()
+  
+  values = result.get('values', [])
 
-    spreadsheetId = '1NQjPVqU991QtFjPAOT1tCV1DwSYT-mGnMihW6UA0rz8'
-    rangeName = 'GPL570!E:O'
-    result = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheetId, range=rangeName).execute()
-    values = result.get('values', [])
+  surv_file = open("GPL570-survival.txt", 'w')
 
-    surv_file = open("GPL570-survival.txt", 'w')
-	
-    if not values:
-        print('No data found.')
-    else:
-        for row in values:
-		for i in xrange(len(row)):
-			surv_file.write(row[i])
-		surv_file.write('\n')
-	surv_file.close()
+  if not values:
+    print('No data found.')
+  else:
+    for row in values:
+      "column"
+      for i in xrange(len(row)):
+        if row[i] != "N/A":
+          surv_file.write(row[i])
+        else:
+          surv_file.write("---")
+        surv_file.write('\t')
+      surv_file.write('\n')
+  
+  surv_file.close()
+
 if __name__ == '__main__':
-    main()
+  main()
 
