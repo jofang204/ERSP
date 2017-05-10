ebba80288b3126e0d268adc3f9c517ad7de2c1fe
ame9yu <kittyqiu.0804@gmail.com>
2017-05-10 16:48:03 -0700

    new branch for automation

--- normalization.sh
+++ normalization.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
 
-raw_dir=./raw/*
+raw_dir=./raw/GPL570/
+
+wanted=`tail -n +2 GPL570-ih.txt | cut -f1`
+
+for id in wanted:
+	ls $raw_dir/$id
 
 # normalization
-for f in $raw_dir
-do
-	echo $f
-	platform=`basename $f`
-	echo $platform
-	python unwanted_list.py $platform > $platform.unwanted
+# for f in $raw_dir
+# do
+# 	echo $f
+# 	platform=`basename $f`
+# 	echo $platform
+# 	python unwanted_list.py $platform > $platform.unwanted
 
-	# untar the raw files
-	for t in $f/*RAW.tar
-	do
-		echo untarring $t
-		tar -xvf $f
-		rm $f
-	done
+# 	raw_tar=./raw/GPL570/*
+
+# 	# # untar the raw files
+# 	# for t in $raw_tar
+# 	# do
+# 	# 	echo $t
+# 	# 	echo "untarring $t"
+# 	# 	tar -xvf $t
+# 	# 	rm $t
+# 	# done
 	
-	# remove the unwanted GSMs
-	for file in `cat $platform.unwanted`
-	do
-		rm ./raw/$platform/$file*
-	done
+# 	# remove the unwanted GSMs
+# 	for file in `cat $platform.unwanted`
+# 	do
+# 		rm ./raw/$platform/$file*
+# 	done
 	
-	rm $platform.unwanted
-	ls ./raw/$platform/*CEL* > filelist
-	bash scr-rma $platform
-done
+# 	rm $platform.unwanted
+# 	ls ./raw/$platform/*CEL* > filelist
+# 	bash scr-rma $platform
+# done
 
 # compile into one expr
-soft_touse=`ls ./soft/GPL570/*soft | head -1`
-python soft_parser.py $soft_touse
-python thre.py
-python info.py
+# soft_touse=`ls ./soft/GPL570/*soft | head -1`
+# python soft_parser.py $soft_touse
+# python thre.py
+# python info.py
 
 
 
