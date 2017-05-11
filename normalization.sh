c0ce77d9f902a449fc5ef6a70cd4a712a0f3caf0
Shijie <florencefsj@gmail.com>
2017-05-10 20:53:31 -0700

    download

--- normalization.sh
+++ normalization.sh
@@ -10,6 +10,27 @@ touch filelist
 for id in $wanted
 do
 	ls $raw_dir/$id* >> filelist
+    baseAddress=ftp://ftp.ncbi.nlm.nih.gov/geo/samples/
+    GSMID=$1
+    flag=true
+
+    for raw_file in `ls $raw_dir`; do
+        if [ ${GSMID}.CEL.gz = ${raw_file} ]
+        then flag=false
+            break
+        fi
+    done
+
+    if [ ${flag} = true ]
+    then
+        Len=${#GSMID}
+        GSMDIR=${GSMID::Len-3}nnn/
+        webLink=${baseAddress}${GSMDIR}${GSMID}/suppl/${GSMID}.CEL.gz
+        wget -O ${raw_dir}/${GSMID}.CEL.gz ${webLink}
+    fi
+
+
+
 done
 
 bash scr-rma $platform
